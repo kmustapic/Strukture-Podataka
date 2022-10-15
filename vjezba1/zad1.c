aa96bc57c0488bd61607122141800194bbf0a9e3
Iva Pleic <ipleic46@gmail.com>
2022-10-15 12:51:02 +0200

    added libraries,main and structure Student

---
+++ vjezba1/zad1.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+#define MAX_NAME 64
+
+
+struct _student;
+typedef struct _student
+{ 
+
+	char ime[MAX_NAME];
+	char prezime[MAX_NAMES];
+	int bodovi;
+	float relBodovi;
+
+} Student
+
+int main() {
+
+
+
+
+    return 0;
+}
