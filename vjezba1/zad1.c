006f2b61eaa88d06a115020d17b60e5a328dc4e7
Iva Pleic <ipleic46@gmail.com>
2022-10-17 18:21:13 +0200

    added functions and added structure in the main function

--- vjezba1/zad1.c
+++ vjezba1/zad1.c
@@ -3,23 +3,32 @@
 #include <stdlib.h>
 #include <malloc.h>
 #define MAX_NAME 64
+#define MAX_LINE 1024
 
-
-struct _student;
-typedef struct _student
+typedef struct
 { 
-
 	char ime[MAX_NAME];
-	char prezime[MAX_NAMES];
+	char prezime[MAX_NAME];
 	int bodovi;
 	float relBodovi;
 
-} Student
+} Student;
+
+int pronadiBrojStudenata(char nazivDatoteke[]);
+Student* AlokacijaMemorije(int brojStudenata,char dat[]);
+int Ispis(int brojStudenata,Student* student);
+int MaxBodovi(int BrojStudenata,Student* student);
 
 int main() {
 
+	char NazivDatoteke[MAX_LINE]="studenti.txt";
+	int BrojStudenata=0;
+	Student* student=NULL;
+	
+	BrojStudenata=pronadiBrojStudenata(NazivDatoteke);
+	student=AlokacijaMemorije(BrojStudenata,NazivDatoteke);
+	Ispis(BrojStudenata,student);
 
-
-
+	free(student);
     return 0;
 }
