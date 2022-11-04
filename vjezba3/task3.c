3d74e3488c258d9ca0a5be5d0ec8154728e5e5f1
Iva Pleic <ipleic46@gmail.com>
2022-11-04 13:31:09 +0100

    Add function for sorting and inserting the list in the file

--- vjezba3/task3.c
+++ vjezba3/task3.c
@@ -1,16 +1,15 @@
 #define CRT_SECURE_NO_WARNINGS
-#define _CRT_SECURE_NO_DEPRECATE  
+#define _CRT_SECURE_NO_DEPRECATE
 #define _CRT_NONSTDC_NO_DEPRECATE
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <windows.h>
-//#include <unistd.h>
 #define MAX_SIZE (50)
 
 struct _person;
-typedef struct _person* PersonP;
+typedef struct _person *PersonP;
 typedef struct _person
 {
     char firstName[MAX_SIZE];
@@ -20,28 +19,33 @@ typedef struct _person
 } Person;
 
 int PrintMenu();
-int EnterValidString(char* string, char* variableName);
-int AddFirst(PersonP head, char* firstName, char* lastName, int birthYear);
-PersonP CreatePerson(char* firstName, char* lastName, int birthYear);
+int EnterValidString(char *string, char *variableName);
+int AddFirst(PersonP head, char *firstName, char *lastName, int birthYear);
+PersonP CreatePerson(char *firstName, char *lastName, int birthYear);
 int InsertAfter(PersonP position, PersonP newPerson);
-int AddLast(PersonP head, char* firstName, char* lastName, int birthYear);
+int AddLast(PersonP head, char *firstName, char *lastName, int birthYear);
 PersonP FindLast(PersonP head);
-int FindByLastName(PersonP first, char* lastName);
-int DeleteAfter(PersonP head, char* lastName);
+int FindByLastName(PersonP first, char *lastName);
+int DeleteAfter(PersonP head, char *lastName);
 int PrintList(PersonP head);
 
-int InsertAfterCertainEl(PersonP head, PersonP newPerson, char* findLastName);
-int InsertBeforeCertainEl(PersonP head, PersonP newPerson, char* findLastName);
+int InsertAfterCertainEl(PersonP head, PersonP newPerson, char *findLastName);
+int InsertBeforeCertainEl(PersonP head, PersonP newPerson, char *findLastName);
+int SortList(PersonP head);
+int WriteInFile(PersonP head, char *fileName);
+char *EnterFileName();
 
 int main()
 {
-    Person Head = { .next = NULL, .firstName = {0}, .lastName = {0}, .birthYear = 0 };
+    Person Head = {.next = NULL, .firstName = {0}, .lastName = {0}, .birthYear = 0};
     PersonP p = &Head;
-    char firstName[MAX_SIZE] = { 0 };
-    char lastName[MAX_SIZE] = { 0 };
-    char searchLastName[MAX_SIZE] = { 0 };
+    char firstName[MAX_SIZE] = {0};
+    char lastName[MAX_SIZE] = {0};
+    char searchLastName[MAX_SIZE] = {0};
     int birthYear = 0;
     int choice = -1;
+    PersonP newPerson = NULL;
+    char *fileName = NULL;
 
     while (choice != 0)
     {
@@ -55,9 +59,9 @@ int main()
         case 1:
             printf(
                 "=================================================================\n"
-                "You chose to add new person to the beginning of list\n\n"
-                "Please enter person's first name: "
-            );
+                "You chose to add new person to the beginning of list\n\n");
+
+            printf("Please enter person's first name: ");
             scanf(" %s", firstName);
             EnterValidString(firstName, "first name");
             printf("Please enter person's last name: ");
@@ -65,21 +69,21 @@ int main()
             EnterValidString(lastName, "last name");
             printf("Please enter person's birth year: ");
             scanf("%d", &birthYear);
+
             AddFirst(p, firstName, lastName, birthYear);
             printf(
                 "\nperson is succesfully added to the beginning of list!\n\n"
                 "Press enter to continue app execution.\n"
-                "=================================================================\n"
-            );
+                "=================================================================\n");
             system("pause");
             break;
 
         case 2:
             printf(
                 "=================================================================\n"
-                "You chose to add new person to the end of list\n\n"
-                "Please enter person's first name: "
-            );
+                "You chose to add new person to the end of list\n\n");
+
+            printf("Please enter person's first name:");
             scanf(" %s", firstName);
             EnterValidString(firstName, "first name");
             printf("Please enter person's last name: ");
@@ -87,12 +91,12 @@ int main()
             EnterValidString(lastName, "last name");
             printf("Please eneter person's birth year: ");
             scanf("%d", &birthYear);
+
             AddLast(p, firstName, lastName, birthYear);
             printf(
                 "\nPerson is succesfully added to the end of list!\n\n"
                 "Press enter to continue app execution.\n"
-                "=================================================================\n"
-            );
+                "=================================================================\n");
             system("pause");
             break;
 
@@ -100,15 +104,13 @@ int main()
             printf(
                 "=================================================================\n"
                 "You chose to search for a person by their last name\n\n"
-                "Please enter person's last name: "
-            );
+                "Please enter person's last name: ");
             scanf(" %s", lastName);
             EnterValidString(lastName, "last name");
             FindByLastName(p, lastName);
             printf(
                 "\nPress enter to continue app execution.\n"
-                "=================================================================\n"
-            );
+                "=================================================================\n");
             system("pause");
             break;
 
@@ -116,37 +118,33 @@ int main()
             printf(
                 "=================================================================\n"
                 "You chose to delete existing person\n\n"
-                "Please enter person's last name: "
-            );
+                "Please enter person's last name: ");
             scanf(" %s", lastName);
             EnterValidString(lastName, "last name");
             DeleteAfter(p, lastName);
             printf(
                 "\nPress enter to continue app execution.\n"
-                "=================================================================\n"
-            );
+                "=================================================================\n");
             system("pause");
             break;
 
         case 5:
             printf(
                 "=================================================================\n"
-                "You chose to print current person list:\n\n"
-            );
+                "You chose to print current person list:\n\n");
             PrintList(p);
             printf(
                 "\n\nPress enter to continue app execution.\n"
-                "=================================================================\n"
-            );
+                "=================================================================\n");
             system("pause");
             break;
 
         case 6:
             printf(
                 "=================================================================\n"
-                "You chose to insert new person after certain person (by surname):\n\n"
-                "Enter last name of person in the list, after who you want to add new person:\n"
-            );
+                "You chose to insert new person after certain person (by surname):\n\n");
+
+            printf("Enter last name of person in the list, after who you want to add new person:");
             scanf(" %s", searchLastName);
             EnterValidString(searchLastName, "searched last name");
 
@@ -158,14 +156,13 @@ int main()
             EnterValidString(lastName, "last name");
             printf("Please enter new person's birth year: ");
             scanf("%d", &birthYear);
-            PersonP newPerson = NULL;
             newPerson = CreatePerson(firstName, lastName, birthYear);
             InsertAfterCertainEl(p, newPerson, searchLastName);
+
             printf(
                 "\nPerson is succesfully added after certain person in the list!\n\n"
                 "\nPress enter to continue app execution.\n"
-                "=================================================================\n"
-            );
+                "=================================================================\n");
             system("pause");
             break;
 
@@ -173,8 +170,7 @@ int main()
             printf(
                 "=================================================================\n"
                 "You chose to insert new person before certain person (by surname):\n\n"
-                "Enter last name of person in the list, before who you want to add new person:\n"
-            );
+                "Enter last name of person in the list, before who you want to add new person:\n");
             scanf(" %s", searchLastName);
             EnterValidString(searchLastName, "searched last name");
 
@@ -186,18 +182,41 @@ int main()
             EnterValidString(lastName, "last name");
             printf("Please enter new person's birth year: ");
             scanf("%d", &birthYear);
-            PersonP newPerson = NULL;
             newPerson = CreatePerson(firstName, lastName, birthYear);
 
-           
+            system("pause");
+            break;
+        case 8:
+            printf(
+                "=================================================================\n"
+                "You chose to sort the list of people(by surname):\n\n");
+            SortList(p);
+            printf(
+                "\nList has been sucesfully sorted!\n\n"
+                "\nPress enter to continue app execution.\n"
+                "=================================================================\n");
+            system("pause");
+            break;
+
+        case 9:
+            printf(
+                "=================================================================\n"
+                "You chose to insert the list of people in the file:\n\n");
+            fileName = EnterFileName();
+            WriteInFile(p, fileName);
+            printf(
+                "\nList has been sucesfully added to the file : %s\n\n", fileName);
+            printf("\nPress enter to continue app execution.\n"
+                   "=================================================================\n");
+            system("pause");
+            break;
         }
     }
     system("cls");
     printf(
         "=================================================================\n"
         "\nYou have exited the application!\n\n"
-        "=================================================================\n"
-    );
+        "=================================================================\n");
     system("pause");
 
     return EXIT_SUCCESS;
@@ -215,14 +234,17 @@ int PrintMenu()
         "    4 - Delete certain person\n"
         "    5 - Print current person list\n"
         "    6 - Inset new person after certain person (by surname)\n"
+        "    7 - Insert new person before certain person(by surname)\n"
+        "    8 - Sort the list of people (by surname)\n"
+        "    9 - Insert the list in the file\n"
+        "    10 -Read the list from the file\n "
         "    0 - Exit application\n"
-        "=================================================================\n"
-    );
+        "=================================================================\n");
 
     return EXIT_SUCCESS;
 }
 
-int EnterValidString(char* string, char* variableName)
+int EnterValidString(char *string, char *variableName)
 {
     if (strcmp(string, "") != 0)
         return EXIT_SUCCESS;
@@ -233,8 +255,8 @@ int EnterValidString(char* string, char* variableName)
         {
             printf(
                 "You entered an empty string!\n"
-                "Please enter new %s: ", variableName
-            );
+                "Please enter new %s: ",
+                variableName);
             scanf("%s", string);
         }
 
@@ -242,7 +264,7 @@ int EnterValidString(char* string, char* variableName)
     }
 }
 
-int AddFirst(PersonP head, char* firstName, char* lastName, int birthYear)
+int AddFirst(PersonP head, char *firstName, char *lastName, int birthYear)
 {
     PersonP newPerson = NULL;
     newPerson = CreatePerson(firstName, lastName, birthYear);
@@ -256,7 +278,7 @@ int AddFirst(PersonP head, char* firstName, char* lastName, int birthYear)
     return EXIT_SUCCESS;
 }
 
-PersonP CreatePerson(char* firstName, char* lastName, int birthYear)
+PersonP CreatePerson(char *firstName, char *lastName, int birthYear)
 {
     PersonP newPerson = NULL;
     newPerson = (PersonP)malloc(sizeof(Person));
@@ -282,7 +304,7 @@ int InsertAfter(PersonP position, PersonP newPerson)
     return EXIT_SUCCESS;
 }
 
-int AddLast(PersonP head, char* firstName, char* lastName, int birthYear)
+int AddLast(PersonP head, char *firstName, char *lastName, int birthYear)
 {
     PersonP newPerson = NULL;
     PersonP last = NULL;
@@ -308,7 +330,7 @@ PersonP FindLast(PersonP head)
     return temp;
 }
 
-int FindByLastName(PersonP first, char* lastName)
+int FindByLastName(PersonP first, char *lastName)
 {
     int flag = 0;
     PersonP temp = NULL;
@@ -329,7 +351,7 @@ int FindByLastName(PersonP first, char* lastName)
     return EXIT_SUCCESS;
 }
 
-int DeleteAfter(PersonP head, char* lastName)
+int DeleteAfter(PersonP head, char *lastName)
 {
     int flag = 0;
     PersonP prev = NULL;
@@ -366,7 +388,7 @@ int PrintList(PersonP head)
     return EXIT_SUCCESS;
 }
 
-int InsertAfterCertainEl(PersonP head, PersonP newPerson, char* findLastName)
+int InsertAfterCertainEl(PersonP head, PersonP newPerson, char *findLastName)
 {
     PersonP temp = NULL;
     temp = head;
@@ -378,11 +400,11 @@ int InsertAfterCertainEl(PersonP head, PersonP newPerson, char* findLastName)
         }
         temp = temp->next;
     }
-   
+
     return EXIT_SUCCESS;
 }
 
-int InsertBeforeCertainEl(PersonP head, PersonP newPerson, char* findLastName)
+int InsertBeforeCertainEl(PersonP head, PersonP newPerson, char *findLastName)
 {
     PersonP prev = NULL;
     PersonP temp = NULL;
@@ -396,5 +418,60 @@ int InsertBeforeCertainEl(PersonP head, PersonP newPerson, char* findLastName)
         temp = temp->next;
     }
 
+    return EXIT_SUCCESS;
+}
+
+int SortList(PersonP p)
+{
+    PersonP a = NULL, b = NULL, prev = NULL, end = NULL;
+    for (a = p; a->next != end;)
+    {
+        prev = a;
+        for (b = a->next; b->next != end; b = b->next)
+        {
+            if (strcmp(b->lastName, b->next->lastName) > 0)
+            {
+                PersonP pom = b->next;
+                prev->next = pom;
+                b->next = pom->next;
+                pom->next = b;
+                b = pom;
+            }
+            prev = b;
+        }
+        end = b;
+    }
+}
+
+char *EnterFileName()
+{
+    char *fileName = NULL;
+    fileName = malloc(sizeof(char) * 20);
+    printf("\nInsert the name of the file you want to add the list to:\n");
+    scanf(" %s", fileName);
+    return fileName;
+}
+
+int WriteInFile(PersonP head, char *fileName)
+{
+
+    FILE *file = NULL;
+    file = fopen(fileName, "w");
+
+    PersonP temp = head->next;
+
+    if (NULL == file)
+    {
+        printf("Unable to open file.");
+        return -1;
+    }
+
+    while (temp)
+    {
+        fprintf(file, "NAME: %s, SURNAME: %s, BIRTHYEAR: %d\n", temp->firstName, temp->lastName, temp->birthYear);
+        temp = temp->next;
+    }
+
+    fclose(file);
     return EXIT_SUCCESS;
 }
