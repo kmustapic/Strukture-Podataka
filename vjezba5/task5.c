e5760adb4425acfe4ba4848c31c53fdcb4804fec
Iva Pleic <ipleic46@gmail.com>
2022-12-07 16:58:17 +0100

    Finished task 5

--- vjezba5/task5.c
+++ vjezba5/task5.c
@@ -12,7 +12,6 @@ typedef struct _stacknode
 } StackNode;
 
 char *EnterFileName();
-
 int CountPostfix(char *fileName, double *result);
 char *ReadFile(char *fileName);
 int StringToPostfix(char *buffer, double *result);
@@ -20,11 +19,8 @@ int Result(StackNodeP head, double *result);
 StackNodeP CreateElement(double number);
 int InsertAfter(StackNodeP head, StackNodeP newElement);
 int PrintPostfix(StackNodeP head);
-
-//=======================================
 int Pop(StackNodeP head, double *result);
 int PopAndOperation(StackNodeP head, char operation, double *result);
-//================================================
 
 int main()
 {
@@ -35,7 +31,7 @@ int main()
 
     fileName = EnterFileName();
     CountPostfix(fileName, &result);
-    printf("Rezultat: %lf\n", &result);
+    printf("Rezultat: %lf\n", result);
 
     return EXIT_SUCCESS;
 }
@@ -81,7 +77,7 @@ char *ReadFile(char *fileName)
     if (!fp)
     {
         perror("Can't open file!\n");
-        return NULL;
+        return EXIT_FAILURE;
     }
 
     fseek(fp, 0, SEEK_END);
@@ -199,6 +195,65 @@ int PrintPostfix(StackNodeP head)
         temp = temp->next;
     }
     printf("\n");
+
+    return EXIT_SUCCESS;
+}
+
+int Pop(StackNodeP head, double* result)
+{
+    StackNodeP temp = NULL;
+
+    temp = head->next;
+    if (!temp)
+    {
+        perror("\nStack is empty.");
+        return EXIT_FAILURE;
+    }
+
+    head->next = temp->next;
+    *result = temp->number;
+    free(temp);
+
+    return EXIT_SUCCESS;
+}
+
+int PopAndOperation(StackNodeP head, char operation, double* result)
+{
+    double operand1 = 0;
+    double operand2 = 0;
+    int status1 = 0;
+    int status2 = 0;
+
+    status1 = Pop(head, &operand1);
+    if (status1 != 0)
+    {
+        return EXIT_FAILURE;
+    }
+
+    status2 = Pop(head, &operand2);
+    if (status2 != 0)
+    {
+        return EXIT_FAILURE;
+    }
+
+    switch (operation)
+    {
+    case '+':
+        *result = operand2 + operand1;
+        break;
+    case '-':
+        *result = operand2 - operand1;
+        break;
+    case '*':
+        *result = operand2 * operand1;
+        break;
+    case '/':
+        *result = operand2 / operand1;
+        break;
+    default:
+        printf("\nThis operation doesn't exists.", operation);
+        return EXIT_FAILURE;
+    }
 
     return EXIT_SUCCESS;
 }
