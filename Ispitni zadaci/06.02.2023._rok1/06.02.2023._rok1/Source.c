ab0d476a41744e0dd83c822625f97705aedfac88
kmustapic <kristina.mustapic10@gmail.com>
2023-02-28 15:19:23 +0100

    Needed fixes for task 06.02.2023._rok1

--- Ispitni zadaci/06.02.2023._rok1/06.02.2023._rok1/Source.c
+++ Ispitni zadaci/06.02.2023._rok1/06.02.2023._rok1/Source.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #define MAX_NAME_LEN (256)
 
 struct _student;
@@ -12,6 +13,7 @@ typedef struct _student {
 	int subject1;
 	int subject2;
 	int subject3;
+	double avg;
 	StudentP next;
 } Student;
 
@@ -24,17 +26,33 @@ int PrintList(StudentP head);
 int DeleteList(StudentP head);
 StudentP FindStud(StudentP head, char* firstName, char* lastName);
 StudentP MergeLists(StudentP head, StudentP toMerge, int whichList);
+// OCJENA 3
+StudentP MergeLists2(StudentP head, StudentP toMerge, int whichList);
+double Avg(StudentP stud);
+int Cmp(StudentP head, StudentP new);
+int InsertSorted(StudentP head, StudentP new);
+int PrintList2(StudentP head);
+// OCJENA 4
+StudentP FindStud2(StudentP head, char* firstName);
+int DeleteStud(StudentP head, StudentP stud);
+int DeleteChosenStud(StudentP head);
+// OCJENA 5
+int GenerateRand(int lower, int upper);
+int MoveStudToFront(StudentP head, StudentP toMove);
+int MoveToFront(StudentP head);
 
 int main() {
 	StudentP head1 = NULL;
 	StudentP head2 = NULL;
 	StudentP head3 = NULL;
 	StudentP headMerge = NULL;
+	StudentP headMerge2 = NULL;
 
 	head1 = InitializeStud();
 	head2 = InitializeStud();
 	head3 = InitializeStud();
 	headMerge = InitializeStud();
+	headMerge2 = InitializeStud();
 
 	char f1[MAX_NAME_LEN] = "subject1.txt";
 	char f2[MAX_NAME_LEN] = "subject2.txt";
@@ -58,19 +76,24 @@ int main() {
 	printf("\nPRINT MERGED LIST:\n");
 	PrintList(headMerge);
 
-	/*
+	MergeLists2(headMerge2, head1, 1);
+	MergeLists2(headMerge2, head2, 2);
+	MergeLists2(headMerge2, head3, 3);
+	printf("\nPRINT SORTED MERGED LIST:\n");
+	PrintList2(headMerge2);
+
+//	DeleteChosenStud(headMerge);
+
+//	MoveToFront(headMerge);
+
 	DeleteList(head1);
 	DeleteList(head2);
 	DeleteList(head3);
 	DeleteList(headMerge);
-	*/
-
+	DeleteList(headMerge2);
+	
 	return EXIT_SUCCESS;
 }
-
-
-
-
 
 StudentP InitializeStud() {
 	StudentP new = NULL;
@@ -84,6 +107,7 @@ StudentP InitializeStud() {
 	new->subject1 = 1;
 	new->subject2 = 1;
 	new->subject3 = 1;
+	new->avg = 0.0;
 	new->next = NULL;
 
 	return new;
@@ -182,9 +206,10 @@ StudentP MergeLists(StudentP head, StudentP toMerge, int whichList) {
 	for (p = toMerge->next; p != NULL; p = p->next) {
 		
 		if (whichList == 1) {
-			if (p->subject1 > 1)
+			if (p->subject1 > 1) {
 				findStud = CreateStud(p->firstName, p->lastName, p->subject1, 1);
 				AddToFront(head, findStud);
+			}
 		}
 
 		else if (whichList == 2) {
@@ -212,29 +237,194 @@ StudentP MergeLists(StudentP head, StudentP toMerge, int whichList) {
 				}
 			}
 		}
-	
 	}
 
 	return head;
 }
 
+double Avg(StudentP stud) {
+	double avg = 1. * (stud->subject1 + stud->subject2 + stud->subject3) / 3;
+	return avg;
+}
+
+int InsertAfter(StudentP head, StudentP new) {
+	new->next = head->next;
+	head->next = new;
+
+	return EXIT_SUCCESS;
+}
+
+//**************
+int Cmp(StudentP head, StudentP new) {
+	int equal = 0;
+	int res = 0;
+	
+	if (fabs(head->avg - new->avg) < 0.00001)
+		equal = 1;
+	if ((head->avg < new->avg) || ( equal == 1 && strcmp(head->lastName, new->lastName) < 0))
+		res = 1;
+
+	return res;
+}
+//**************
+
+int InsertSorted(StudentP head, StudentP new) {
+	StudentP p = head;
+	
+	while (NULL != p->next && Cmp(p->next, new) == 1)
+		p = p->next;
+	InsertAfter(p, new);
+
+	return EXIT_SUCCESS;
+}
+
+StudentP MergeLists2(StudentP head, StudentP toMerge, int whichList) {
+	StudentP p = NULL;
+	StudentP findStud = NULL;
+
+	for (p = toMerge->next; p != NULL; p = p->next) {
+
+		if (whichList == 1) {
+			if (p->subject1 > 1) {
+				findStud = CreateStud(p->firstName, p->lastName, p->subject1, 1);
+				InsertSorted(head, findStud);
+			}
+		}
+
+		else if (whichList == 2) {
+			if (p->subject2 > 1) {
+				findStud = FindStud(head, p->firstName, p->lastName);
+
+				if (NULL != findStud)
+					findStud->subject2 = p->subject2;
+				else {
+					findStud = CreateStud(p->firstName, p->lastName, p->subject2, 2);
+					InsertSorted(head, findStud);
+				}
+			}
+		}
+
+		else if (whichList == 3) {
+			if (p->subject3 > 1) {
+				findStud = FindStud(head, p->firstName, p->lastName);
+
+				if (NULL != findStud)
+					findStud->subject3 = p->subject3;
+				else {
+					findStud = CreateStud(p->firstName, p->lastName, p->subject3, 3);
+					InsertSorted(head, findStud);
+				}
+			}
+		}
+	}
+
+	return head;
+}
+
+int PrintList2(StudentP head) {
+	StudentP temp = head->next;
+	double avg = 0;
+	while (NULL != temp) {
+		avg = Avg(temp);
+		printf("\t- %s %s (%d, %d, %d) -\t\t%.3f\n", temp->lastName, temp->firstName, temp->subject1, temp->subject2, temp->subject3, avg);
+
+		temp = temp->next;
+	}
+
+	return EXIT_SUCCESS;
+}
+
+StudentP FindStud2(StudentP head, char* firstName) {
+	StudentP p = head->next;
+	while (NULL != p) {
+		if (strcmp(p->firstName, firstName) == 0)
+			return p;
+		p = p->next;
+	}
+
+	return NULL;
+}
 
 
+//**********************
+int DeleteStud(StudentP head, StudentP stud) {
+	StudentP temp = head;
+	StudentP toDelete = NULL;
 
+	if (NULL == temp) {
+		head = head->next;
+		temp->next = NULL;
+		free(temp);
+	}
+	else
+		for (temp = head->next; NULL != temp; temp = temp->next) {
+			toDelete = temp->next;
+			if (strcmp(toDelete->firstName, stud->firstName) == 0) {
+				temp->next = temp->next->next;
+				toDelete->next = NULL;
+				free(toDelete);
+			}
+		}
 
+	return EXIT_SUCCESS;
+}
 
+int DeleteChosenStud(StudentP head) {
+	printf("\n\nDelete student(s): NAME\n");
+	char* name[MAX_NAME_LEN] = { 0 };
+	StudentP t = NULL;
+	printf("\nDelete student(s): ");
+	scanf("%s", name);
+	t = FindStud2(head, name);
+//
+	printf("****FOUND STUD: %s\n", t->firstName);
+//
+	if(NULL == t)
+		printf("\nStudent with name %s doesn't exist!\n", name);
+	else {
+		for (t = head->next; t != NULL; t = t->next) {
+			if (strcmp(t->firstName, name) == 0) {
+				DeleteStud(head, t);
+			}
+		}
+		printf("\nStudent(s) with name: %s successfully deleted!\n");
+		printf("LIST AFTER DELETING:\n");
+		PrintList(head);
+		}
 
+	return EXIT_SUCCESS;
+}
 
+int GenerateRand(int lower, int upper) {
+	int n;
+	n = (rand() % (upper - lower + 1)) + lower;
 
+	return n;
+}
 
+int MoveStudToFront(StudentP head, StudentP toMove) {
+	StudentP p = NULL;
+	p = toMove;
+	p->next = head->next;
+	head->next = p;
+	toMove->next = NULL;
+	free(toMove);
 
+	return EXIT_SUCCESS;
+}
 
+int MoveToFront(StudentP head) {
+	int grade;
+	StudentP t = NULL;
 
+	grade = GenerateRand(1, 5);
+	printf("\nGENERATED GRADE: %d\n", grade);
+	for (t = head->next; NULL != t; t = t->next)
+		if (t->subject1 == grade || t->subject2 == grade || t->subject3 == grade)
+			MoveStudToFront(head, t);
 
+	printf("\nPRINT FINAL LIST: ");
+	PrintList(head);
 
-
-
-
-
-
-
+	return EXIT_SUCCESS;
+}
